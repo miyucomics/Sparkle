aaec8e69064188d9f3f97b4afe2e5e684c7033b3
Terra Brown <superloach42@gmail.com>
2022-06-07 08:14:06 -0400

    update for 1.18.2
    also bumped version to 1.0.2

--- src/main/java/briancomics/sparkle/SparkleMain.java
+++ src/main/java/briancomics/sparkle/SparkleMain.java
@@ -2,15 +2,14 @@ package briancomics.sparkle;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 import net.minecraft.particle.DefaultParticleType;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class SparkleMain implements ModInitializer {
-	public static final Tag<Block> SPARKLY_BLOCKS = TagFactory.BLOCK.create(new Identifier("sparkle", "sparkly"));
+	public static final TagKey<Block> SPARKLY_BLOCKS = TagKey.of(Registry.BLOCK_KEY, new Identifier("sparkle", "sparkly"));
 	public static DefaultParticleType SPARKLE_PARTICLE;
 
 	@Override
